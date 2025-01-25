92683840ec92a25f8a078722112637cbd1f1d223
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-25 10:27:09 +0900

    refactor: Change mixin to staticmethod
    - Database fetcher vender determination will be provided by class
      method.
    
    Mixin couples with original class, which harms extensibility. To provide
    vender determination to other functions outside the view, determinating
    should be provided by service layer.

--- mark/views/fetcher_vendor_determiner_service.py
+++ mark/views/fetcher_vendor_determiner_service.py
@@ -1,8 +1,9 @@
 from .database_fetcher import DatabaseFetcher, PostgresqlFetcher
 from submit.models import Guess
 
-class FetcherVendorDeterminerMixin(object):
-    def determine_vendor(self, guess: Guess) -> DatabaseFetcher:
+class FetcherVendorDeterminerService(object):
+    @classmethod
+    def determine_vendor(cls, guess: Guess) -> DatabaseFetcher:
         vendor_name = guess.selected_vendor.system_name
 
         if vendor_name == 'postgresql':
