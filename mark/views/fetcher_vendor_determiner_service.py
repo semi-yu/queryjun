56c5a6dcbad6f2e0b61e07894bded7fe0ac02557
armedHeistKino <urban.manul.1919@gmail.com>
2025-01-25 16:52:26 +0900

    refactor: Remove unneccessary inheritance

--- mark/views/fetcher_vendor_determiner_service.py
+++ mark/views/fetcher_vendor_determiner_service.py
@@ -3,7 +3,7 @@ from submit.models import Guess
 from mark.views.fetchers.database_fetcher import DatabaseFetcher
 from mark.views.fetchers.postgresql_fetcher import PostgresqlFetcher
 
-class FetcherVendorDeterminerService(object):
+class FetcherVendorDeterminerService:
     @classmethod
     def determine(cls, guess: Guess) -> DatabaseFetcher:
         vendor_name = guess.selected_vendor.system_name
