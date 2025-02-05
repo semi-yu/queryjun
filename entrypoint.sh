e485e09ea4517f572fdfa4d3e05c3f2a0b4b5e9d
armedHeistKino <urban.manul.1919@gmail.com>
2025-02-05 16:16:29 +0900

    refactor: Remove comments, adding comments are TBD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 set -e
 
 apt-get update && apt-get install -y gettext
-
 envsubst < /docker-entrypoint-initdb.d/init.sql > /docker-entrypoint-initdb.d/init_prepared.sql
 
-# PostgreSQL 초기화 실행
 exec docker-entrypoint.sh postgres
